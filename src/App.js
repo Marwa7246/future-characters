c3ea56a42b4f76a5c31cba9ddbc7bfc551ed696d
Marwa7246 <marwa.ragheb@gmail.com>
2020-11-12 22:16:23 -0500

    install fuse.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Fuse from 'fuse.js'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(Fuse)
   return (
     <div className="App">
       <header className="App-header">
